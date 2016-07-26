bb9b716c51428a6ef6462c2c382d257589f3bbf1
Peter Verheecke <pverheecke@gmail.com>
2016-07-26 12:50:09 -0700

    Just running through a quick tutorial

--- app/src/main/java/net/verheecke/peter/reminders/MainActivity.java
+++ app/src/main/java/net/verheecke/peter/reminders/MainActivity.java
@@ -1,5 +1,6 @@
 package net.verheecke.peter.reminders;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,8 +9,10 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    public final static String EXTRA_MESSAGE = "net.verheecke.peter.reminders.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +29,14 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 
     @Override
