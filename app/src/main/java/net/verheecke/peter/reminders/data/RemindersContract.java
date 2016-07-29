ad40bb5d7829492c99783689998fdbf8cf8ad4f1
pverheecke <pverheecke@gmail.com>
2016-07-28 18:13:33 -0700

    First draft of reminders contract

--- app/src/main/java/net/verheecke/peter/reminders/data/RemindersContract.java
+++ app/src/main/java/net/verheecke/peter/reminders/data/RemindersContract.java
@@ -1,7 +1,76 @@
 package net.verheecke.peter.reminders.data;
 
+import android.content.ContentResolver;
+import android.content.ContentUris;
+import android.net.Uri;
+import android.provider.BaseColumns;
+
 /**
  * Created by Peter on 7/28/2016.
  */
 public class RemindersContract {
+
+    // The "Content authority" is a name for the entire content provider, similar to the
+    // relationship between a domain name and its website.  A convenient string to use for the
+    // content authority is the package name for the app, which is guaranteed to be unique on the
+    // device.
+    public static final String CONTENT_AUTHORITY = "net.verheecke.peter.reminders";
+
+    // Use CONTENT_AUTHORITY to create the base of all URI's which apps will use to contact
+    // the content provider.
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
+    // Possible paths (appended to base content URI for possible URI's)
+    // For instance, content://com.example.android.sunshine.app/weather/ is a valid path for
+    // looking at weather data. content://com.example.android.sunshine.app/givemeroot/ will fail,
+    // as the ContentProvider hasn't been given any information on what to do with "givemeroot".
+    // At least, let's hope not.  Don't be that dev, reader.  Don't be that dev.
+    public static final String PATH_REMINDER = "reminder";
+    public static final String PATH_USER = "user";
+
+
+    public static final class ReminderEntry implements BaseColumns {
+        public static final String TABLE_NAME = "reminder";
+        public static final String COLUMN_REMINDER_ID = "reminder_id";
+        public static final String COLUMN_USER_ID = "user_id";
+        public static final String COLUMN_TITLE = "title";
+        public static final String COLUMN_DESCRIPTION = "description";
+        public static final String COLUMN_DATE_CREATED = "date_created";
+        public static final String COLUMN_REPEAT_ID = "repeat_id";
+        public static final String COLUMN_DAYS_NOTIFY = "days_expiry";
+        public static final String COLUMN_TIME_NOTIFY = "time_expiry";
+        public static final String COLUMN_TAGS = "tags";
+
+        public static final Uri CONTENT_URI =
+                BASE_CONTENT_URI.buildUpon().appendPath(PATH_REMINDER).build();
+
+        public static final String CONTENT_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_REMINDER;
+        public static final String CONTENT_ITEM_TYPE =
+                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_REMINDER;
+
+        public static Uri buildReminderUri(long id) {
+            return ContentUris.withAppendedId(CONTENT_URI, id);
+        }
+    }
+
+    public static final class UserEntry implements BaseColumns {
+        public static final String TABLE_NAME = "user";
+        public static final String COLUMN_USER_ID = "user_id";
+        public static final String COLUMN_DISPLAY_NAME = "display_name";
+        public static final String COLUMN_EMAIL = "email";
+
+        public static final Uri CONTENT_URI =
+                BASE_CONTENT_URI.buildUpon().appendPath(PATH_USER).build();
+
+        public static final String CONTENT_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_REMINDER;
+        public static final String CONTENT_ITEM_TYPE =
+                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_REMINDER;
+
+        public static Uri buildUserUri(long id) {
+            return ContentUris.withAppendedId(CONTENT_URI, id);
+        }
+
+    }
 }
